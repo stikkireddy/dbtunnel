94da27e0209da9ba7f52b5a80025757217e38d71
stikkireddy <stikkireddy>
2023-11-29 19:30:16 +0000

    added nicegui example

---
+++ examples/nicegui/nicegui-example.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# MAGIC %pip install "git+https://github.com/stikkireddy/dbtunnel.git#egg=dbtunnel[nicegui]"
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from nicegui import ui
+from nicegui.events import ValueChangeEventArguments
+
+def show(event: ValueChangeEventArguments):
+    name = type(event.sender).__name__
+    ui.notify(f'{name}: {event.value}')
+
+ui.button('Button', on_click=lambda: ui.notify('Click'))
+with ui.row():
+    ui.checkbox('Checkbox', on_change=show)
+    ui.switch('Switch', on_change=show)
+ui.radio(['A', 'B', 'C'], value='A', on_change=show).props('inline')
+with ui.row():
+    ui.input('Text input', on_change=show)
+    ui.select(['One', 'Two'], value='One', on_change=show)
+ui.link('And many more...', '/documentation').classes('mt-8')
+
+# COMMAND ----------
+
+from dbtunnel import dbtunnel
+dbtunnel.nicegui(ui).run()
+
+# COMMAND ----------
+
+
