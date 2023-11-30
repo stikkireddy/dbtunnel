ad88fd07390c1c4a6950cea1e54d3714199f28c7
stikkireddy <stikkireddy>
2023-11-30 20:19:07 +0000

    added dash example

---
+++ examples/dask/dask-example.py
@@ -0,0 +1,39 @@
+# Databricks notebook source
+# MAGIC %pip install "git+https://github.com/stikkireddy/dbtunnel.git#egg=dbtunnel[dash]"
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash("app")
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+# COMMAND ----------
+
+from dbtunnel import dbtunnel
+dbtunnel.dash(app).run()
+
+# COMMAND ----------
+
+
