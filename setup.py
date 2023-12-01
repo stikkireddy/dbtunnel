97e578cd15cd7313c4abf45c486452bb36e9bdd4
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2023-12-01 08:40:01 -0500

    updated build to 0.1.1 to patch disclaimer and remove old pypi artifact

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dbtunnel",
-    version="0.1.0",
+    version="0.1.1",
     author="Sri Tikkireddy",
     author_email="sri.tikkireddy@databricks.com",
     description="Run app and get cluster proxy url for it in databricks clusters",
