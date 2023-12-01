285ecff9f2892947c65ec536d9db8297c29f9f13
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2023-12-01 08:31:49 -0500

    updated makefile and setup.py

--- Makefile
+++ Makefile
@@ -1,3 +1,17 @@
 build:
-	pip install wheel
-	python setup.py bdist_wheel
+	@echo "Cleaning build..."
+	@rm -rf dist build
+	@echo "Cleaned dist and build..."
+	@echo "Building wheel..."
+	@pip install wheel
+	@echo "Build finished..."
+	@echo "Making distributions..."
+	@python setup.py bdist_wheel sdist
+	@echo "Finished making distributions..."
+
+upload:
+	@echo "Uploading to PyPI..."
+	@twine upload dist/*
+	@echo "Finished uploading to PyPI..."
+
+.PHONY: build
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="dbtunnel",
     version="0.1.0",
     author="Sri Tikkireddy",
     author_email="sri.tikkireddy@databricks.com",
     description="Run app and get cluster proxy url for it in databricks clusters",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/stikkireddy/dbtunnel",
     packages=find_packages(),
     install_requires=[
-
     ],
     # TODO: start refactoring nest_asyncio only when async.run is needed
     extras_require={
