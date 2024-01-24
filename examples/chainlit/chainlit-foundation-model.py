9cbfb4f49f969bd00e7ea648ff42e8dad01eb8e6
stikkireddy <stikkireddy>
2024-01-24 19:41:55 +0000

    simplified notebook

--- examples/chainlit/chainlit-foundation-model.py
+++ examples/chainlit/chainlit-foundation-model.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-!pip install dbtunnel[asgiproxy,chainlit] databricks-genai-inference langchain-community tiktoken langchain mlflow-skinny chromadb rank_bm25 simsimd
+# MAGIC %pip install dbtunnel[asgiproxy,chainlit] databricks-genai-inference langchain-community tiktoken langchain mlflow-skinny chromadb rank_bm25 simsimd
 
 # COMMAND ----------
 
@@ -15,8 +15,7 @@ script_path = current_directory + "/chainlit_foundation_model.py"
 # COMMAND ----------
 
 from dbtunnel import dbtunnel
-
-dbtunnel.chainlit(script_path, port=8988).inject_auth().run()
+dbtunnel.chainlit(script_path).inject_auth().run()
 
 # COMMAND ----------
 
