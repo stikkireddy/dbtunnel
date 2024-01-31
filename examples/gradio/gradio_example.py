7de54d8dfd4486eeb1012211c1a2cef2148a1861
stikkireddy <stikkireddy>
2024-01-31 13:48:52 +0000

    added gradio file example

---
+++ examples/gradio/gradio_example.py
@@ -0,0 +1,41 @@
+import gradio as gr
+import os
+
+
+def combine(a, b):
+    return a + " " + b
+
+
+def mirror(x):
+    return x
+
+
+with gr.Blocks() as demo:
+
+    txt = gr.Textbox(label="Input", lines=2)
+    txt_2 = gr.Textbox(label="Input 2")
+    txt_3 = gr.Textbox(value="", label="Output")
+    btn = gr.Button(value="Submit")
+    btn.click(combine, inputs=[txt, txt_2], outputs=[txt_3])
+
+    with gr.Row():
+        im = gr.Image()
+        im_2 = gr.Image()
+
+    btn = gr.Button(value="Mirror Image")
+    btn.click(mirror, inputs=[im], outputs=[im_2])
+
+    gr.Markdown("## Text Examples")
+    gr.Examples(
+        [["hi", "Adam"], ["hello", "Eve"]],
+        [txt, txt_2],
+        txt_3,
+        combine,
+        cache_examples=True,
+    )
+
+if __name__ == "__main__":
+  demo.queue().launch(
+    server_name=os.getenv("GRADIO_SERVER_NAME"), 
+    server_port=int(os.getenv("GRADIO_SERVER_PORT")),
+  )
