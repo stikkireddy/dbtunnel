981cb252659bab45d71fe91f449eca217c6f01b5
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2024-02-15 14:31:27 -0500

    socket io fix worked removing print statements

--- dbtunnel/vendor/asgiproxy/proxies/websocket.py
+++ dbtunnel/vendor/asgiproxy/proxies/websocket.py
@@ -106,8 +106,8 @@ async def proxy_websocket(
             scope["path"] = scope["path"].replace(root_path, "")
 
     q_string = scope.get("query_string", None)
-    # ensure query params its important for socketio during websocket upgrade
-    print(type(scope["path"]), type(q_string), scope)
+
+    # ensure query params it is important for socketio during websocket upgrade
     if q_string is not None and q_string.decode("utf-8") not in scope["path"]:
         scope["path"] = scope["path"] + "?" + q_string.decode("utf-8")
 
