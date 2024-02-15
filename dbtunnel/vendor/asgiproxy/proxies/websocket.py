887758d8e0cf0c395dab0bb2c031e06306d5830f
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2024-02-15 14:19:21 -0500

    added socketio query param

--- dbtunnel/vendor/asgiproxy/proxies/websocket.py
+++ dbtunnel/vendor/asgiproxy/proxies/websocket.py
@@ -104,10 +104,12 @@ async def proxy_websocket(
         root_path = scope["root_path"]
         if scope["path"].startswith(root_path):
             scope["path"] = scope["path"].replace(root_path, "")
-        if scope.get("query_string") is not None:
-            scope["path"] = scope["path"] + "?" + scope["query_string"].decode("utf-8")
 
-    print(scope)
+    q_string = scope.get("query_string", None)
+    # ensure query params its important for socketio during websocket upgrade
+    if q_string is not None and q_string not in scope["path"]:
+        scope["path"] = scope["path"] + "?" + q_string.decode("utf-8")
+
 
     client_ws: Optional[WebSocket] = None
     upstream_ws: Optional[ClientWebSocketResponse] = None
