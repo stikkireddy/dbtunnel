f2aba1bd0ad5121d3b33ce9cb74d0a2b46663d8e
natefleming-db <157608612+natefleming-db@users.noreply.github.com>
2024-06-14 14:18:50 -0400

    make server blocking

--- dbtunnel/ray.py
+++ dbtunnel/ray.py
@@ -13,6 +13,7 @@ class RayAppTunnel(DbTunnel):
             import uvicorn
             import nest_asyncio
             import ray
+
         except ImportError as e:
             self._log.info("ImportError: Make sure you have ray, fastapi and nest_asyncio installed;"
                   "pip install ray, fastapi nest_asyncio uvicorn")
@@ -21,16 +22,26 @@ class RayAppTunnel(DbTunnel):
     def _run(self):
         self.display()
         self._log.info("Starting server...")
+        import ray
         from ray import serve
+        from ray.serve.handle import DeploymentHandle
         from ray.serve.config import HTTPOptions
 
-        def start():
+
+        # uvicorn.run(app, host="0.0.0.0", port=self._port)
+        # Start the server
+        async def start():
             http_options: HTTPOptions = HTTPOptions(host="0.0.0.0", port=self._port)
             serve.start(http_options=http_options)
-            serve.run(self._ray, route_prefix=self._proxy_settings.url_base_path.rstrip("/"))
+            _: DeploymentHandle = (
+                serve.run(self._ray, route_prefix=self._proxy_settings.url_base_path.rstrip("/"), is_blocking=True)
+            )
+        
 
+        # Run the asyncio event loop instead of uvloop to enable re entrance
+        import asyncio
         self._log.info(f"Use this link: \n{self._proxy_settings.proxy_url}")
-        start()
+        asyncio.run(start())
 
     def _display_url(self):
         # must end with a "/" for it to not redirect
